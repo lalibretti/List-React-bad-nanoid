e0771d9ad1338169cf43bbf4da6b592d3f7a1503
Lauren Libretti <lalibretti616@gmail.com>
2020-11-18 21:52:03 -0500

    End of day

--- list-proj/src/App.js
+++ list-proj/src/App.js
@@ -1,4 +1,6 @@
+import React, {useState} from "react";
 import './App.css';
+import {nanoid} from "nanoid";
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
@@ -7,7 +9,8 @@ import Todo from './components/Todo';
 
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks.map(task => (
     <Todo 
     id={task.id} 
     name={task.name} 
@@ -16,46 +19,24 @@ function App(props) {
     />
   )
   );
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid() , name:name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+
   return (
     <div className="todoapp stack-large">
       <h1>To Do List</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor='new-todo-input' className='label__lg'>
-            What's on your list? 
-          </label>
-        </h2>
-        <input
-          type="text"
-          id = 'new-todo-input'
-          className='input input__lg'
-          name='text'
-          autoComplete='off'
-        />
-        <button type='submit' className='btn btn_primary btn__lg'>
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed='true'>
-          <span className="visually-hidden">Show</span>
-          <span>all</span>
-          <span className="visually-hidden">tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed='false'>
-        <span className="visually-hidden">Show</span>
-          <span>Active</span>
-          <span className="visually-hidden">tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed='false'>
-        <span className="visually-hidden">Show</span>
-          <span>Completed</span>
-          <span className="visually-hidden">tasks</span>
-        </button>
+       <FilterButton />
+       <FilterButton />
+       <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
       role='list'
       className="todo-list stack-large stack-exception"
--- list-proj/src/components/Form.js
+++ list-proj/src/components/Form.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function Form (props) {
+    const [name, setName] = useState('');
+
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault ();
+        props.addTask(name);
+        setName("");
+    }
+
 return(
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <h2 className='label-wrapper'>
             <label htmlFor="new-todo-input"className='label__lg'>
                 What's on your list?
@@ -15,6 +28,8 @@ return(
         className='input input__lg'
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
         />
         <button type='submit'className='btn btn__primary btn__lg'>
             Add
